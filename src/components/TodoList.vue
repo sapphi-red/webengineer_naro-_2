f2c6c12513b687970f5e66001b407e8f30187345
sapphi-red <green@sapphi.red>
2019-05-10 19:07:49 +0900

    Update: おおまかな外枠

---
+++ src/components/TodoList.vue
@@ -0,0 +1,40 @@
+<template>
+  <div>
+    <div class="todos" v-for="todo in todos" :key="todo.name">
+      <div class="todo">
+        <div class="name">{{ todo.name }}</div>
+      </div>
+    </div>
+    <div>
+      <p>Todoを追加する</p>
+      <label>
+        内容
+        <input type="text" v-model="newTodoName">
+      </label>
+      <button @click="addTodo">add</button>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "TodoList",
+  data() {
+    return {
+      todos: [],
+      newTodoName: "",
+    };
+  },
+  methods: {
+    addTodo() {
+      this.todos.push({
+        name: this.newTodoName,
+      });
+      this.newTodoName = "";
+    }
+  }
+};
+</script>
+
+<style>
+</style>
